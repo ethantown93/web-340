49fb01db2b3ad825a729c05c04abad9fed504b15
Ethan <EETfitness@gmail.com>
2019-02-19 12:55:45 -0800

    adding assignment 1.4

---
+++ week-1/townsend-exercise-1.4.js
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title:  townsend-exercise-1.4
+; Author: Professor Krasso
+; Date:   19 February 2019
+; Modified By: Ethan Townsend
+; Description:  Assignment 1.4
+;===========================================
+*/
+
+console.log('You are not in Townsend\'s world!');
