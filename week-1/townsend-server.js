f243a1251a67650dc0b4cbb88f1c2992f436b95e
Ethan <EETfitness@gmail.com>
2019-02-24 11:09:50 -0800

    adding server

--- week-1/townsend-server.js
+++ week-1/townsend-server.js
@@ -1,9 +1,26 @@
+//Header
+
+var header = require('../header.js');
+console.log(header.display("Ethan", "Townsend", "Exercise 1.3"));
+console.log('\n');
+
+/*
+============================================
+; Title:  townsend-server.js
+; Author: Professor Krasso
+; Date:   24 February 2019
+; Modified By: Ethan Townsend
+; Description: creating my first server
+;===========================================
+*/
+
 //Variable declaration:
 
 var http = require("http");
 
 function processRequest(req, res) {
 
+// server body message
 var body = "Hello! And welcome to my very first NodeJs server, created for WEB-340.";
 
     var contentLength = body.length;
