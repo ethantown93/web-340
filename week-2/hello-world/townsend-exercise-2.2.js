b009de201430882c483b7971568ec2cc6c42fcbd
Ethan <EETfitness@gmail.com>
2019-02-27 14:13:56 -0800

    updating

--- week-2/hello-world/townsend-exercise-2.2.js
+++ week-2/hello-world/townsend-exercise-2.2.js
@@ -1,6 +1,18 @@
+/*
+============================================
+; Title:  townsend-exercise-2.2
+; Author: Professor Krasso
+; Date:   26 February 2019
+; Modified By: Ethan Townsend
+; Description:  Assignment 2.2
+;===========================================
+*/
+
+// requiring the express and http modules
 var express = require('express');
 var http = require('http');
 
+// assigning the express function to a variable
 var app = express();
 
 app.use(function(request, response){
@@ -9,6 +21,7 @@ app.use(function(request, response){
     response.end('Hello World\n');
 })
 
+// creating my server
 http.createServer(app).listen(3000, function() {
     console.log('Application started on port: ', 3000);
 });
