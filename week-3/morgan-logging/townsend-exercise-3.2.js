3d68a867ea8b20078ebcb0f0ce1471e60b3c2bd0
Ethan <EETfitness@gmail.com>
2019-03-05 18:16:59 -0800

    adding assignment 3.2

---
+++ week-3/morgan-logging/townsend-exercise-3.2.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+// specifying the route to views
+app.set('views', path.resolve(__dirname, 'views'));
+
+// telling express to use the view engine
+app.set('view engine', 'ejs');
+
+// adding the logger
+app.use(logger('short'));
+
+// creating a git request that will return a response
+app.get('/', function (req, res){
+    res.render('index', {
+        message: 'Testing testin 1, 2, 3. This is a test!'});
+    });
+
+http.createServer(app).listen(8080, function(){
+    console.log('Application started on port 8080');
+});
