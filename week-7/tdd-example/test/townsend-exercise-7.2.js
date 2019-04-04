7037366723725d2cf43073fa028de16d9ca91b98
Ethan Townsend <eetfitness@gmail.com>
2019-04-04 10:28:30 -0700

    adding week 7 assignments

---
+++ week-7/tdd-example/test/townsend-exercise-7.2.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('String#split', function(){
+    it('should return an array of fruits', function() {
+        assert(Array.isArray('Apple,Orange,Mango'.split(',')));
+    });
+});
+
+//Run the test
+
+ //       npm test
+
+//Passed function
+
+function getFruits(str) {
+
+    return str.split(',');
+
+   }
+
+   module.exports = getFruits;
