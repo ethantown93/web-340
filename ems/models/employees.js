5d19c11bbd5255a58d02d904d51062d60a16340f
Ethan Townsend <eetfitness@gmail.com>
2019-04-04 10:55:39 -0700

    adding week 7 assignments

---
+++ ems/models/employees.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  fruit.js
+; Author: Ethan Townsend
+; Date:   4 April 2019
+; Description: File for the Fruit database model
+;===========================================
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let EmployeeSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type:String, required: true}
+});
+
+module.exports = mongoose.model('Employees', EmployeeSchema);
